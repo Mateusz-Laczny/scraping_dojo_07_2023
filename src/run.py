638d1dee6ecc021bc2c0edb9190b0772bad3c23a
Mateusz <mateusz.laczny@interia.pl>
2023-07-10 18:20:07 +0200

    feat: Implemented CLI interface and ability to use either Chrome or Firefox webdriver

--- src/run.py
+++ src/run.py
@@ -1,8 +1,21 @@
-from environment import input_url, output_file
-from jsonl import write_as_jsonl_file
-from selenium_scrapping import SeleniumQuoteScrapper
+import argparse
 
-if __name__ == '__main__':
-    scrapper = SeleniumQuoteScrapper()
+from environment import input_url, output_file, proxy
+from jsonl import write_as_jsonl_file
+from selenium_scrapping import SeleniumQuoteScrapper, Webdriver
+
+parser = argparse.ArgumentParser(description="Quotes webscrapper")
+parser.add_argument("-b", "--browser", type=str, choices=["chrome", "firefox"], help='Browser to use', default='chrome')
+parser.add_argument("-p", "--proxy",action="store_true", help='Flag indicating whether to use proxy')
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    webdriver_to_use = Webdriver(args.browser)
+
+    if args.proxy:
+        scrapper = SeleniumQuoteScrapper(proxy=proxy, webdriver_to_use=webdriver_to_use)
+    else:
+        scrapper = SeleniumQuoteScrapper(webdriver_to_use=webdriver_to_use)
+
     quotes = scrapper.scrape(input_url)
     write_as_jsonl_file(output_file, [quote.as_dict() for quote in quotes])
--- src/selenium_scrapping.py
+++ src/selenium_scrapping.py
@@ -1,24 +1,49 @@
+from enum import Enum
+
 from selenium import webdriver
 from selenium.common import NoSuchElementException
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver import Proxy
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.proxy import ProxyType
+from selenium.webdriver.firefox.service import Service as FirefoxService
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
 
 from quotes import Quote
 
 
+class Webdriver(Enum):
+    CHROME = 'chrome'
+    FIREFOX = 'firefox'
+
+
 class SeleniumQuoteScrapper:
 
-    def __init__(self, proxy: str = None) -> None:
+    def __init__(self, proxy: str = None, webdriver_to_use=None) -> None:
+        if webdriver_to_use is None:
+            webdriver_to_use = Webdriver.CHROME
         self.proxy = proxy
-        options = Options()
-        if proxy is not None:
-            options.add_argument(f'--proxy-server={self.proxy}')
 
-        self.driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
+        if webdriver_to_use == Webdriver.CHROME:
+            options = webdriver.ChromeOptions()
+            if proxy is not None:
+                options.add_argument(f'--proxy-server={self.proxy}')
+
+            self.driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
+        elif webdriver_to_use == Webdriver.FIREFOX:
+            options = webdriver.FirefoxOptions()
+            if proxy is not None:
+                proxy_addr = proxy[:len(proxy) - 5]
+                proxy_port = proxy[len(proxy) - 4:]
+                options.set_preference('network.proxy.type', 1)
+                options.set_preference('network.proxy.socks', proxy_addr)
+                options.set_preference('network.proxy.socks_port', proxy_port)
+                options.set_preference('network.proxy.socks_remote_dns', False)
+
+            self.driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()), options=options)
 
     def scrape(self, start_url: str) -> list[Quote]:
         self.driver.get(start_url)
@@ -46,5 +71,7 @@ class SeleniumQuoteScrapper:
                 next_button_element.click()
             except NoSuchElementException:
                 reached_end = True
+
+        self.driver.quit()
 
         return quotes_list
